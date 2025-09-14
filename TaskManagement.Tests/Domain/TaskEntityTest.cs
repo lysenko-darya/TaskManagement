44e2b208abbd8b2bbb9e5674ea69bc0da66080f5
daria_lisenko daria_lisenko <lisenko-darya@yandex.ru>
2025-09-14 15:54:59 +0300

    task tests

--- TaskManagement.Tests/Domain/TaskEntityTest.cs
+++ TaskManagement.Tests/Domain/TaskEntityTest.cs
@@ -1,24 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xunit;
+﻿using FluentAssertions;
+using TaskManagement.Contracts.Enums;
+using TaskManagement.Domain.Entities;
+using TaskManagement.Domain.Exceptions;
 
 namespace TaskManagement.Tests.Domain;
-
 public class TaskEntityTest
 {
-    //[Fact]
-    //public void Create_task_item_success()
-    //{
-    //    //Arrange    
+    [Theory]
+    [InlineData(Status.New)]
+    [InlineData(Status.InProgress)]
+    [InlineData(Status.Done)]
+    public void SetStatus_From_New_Success(Status newStatus)
+    {
+        //Arrange    
+        var task = new TaskEntity("Author", status: Status.New);
+        //Act 
+        task.SetStatus(newStatus);
+        //Assert
+        task.Status.Should().Be(newStatus);
+    }
 
+    [Theory]
+    [InlineData(Status.New)]
+    [InlineData(Status.InProgress)]
+    [InlineData(Status.Done)]
+    public void SetStatus_From_Done_Fail(Status newStatus)
+    {
+        //Arrange    
+        var task = new TaskEntity("Author", status: Status.Done);
+        //Act          
+        var func = () => task.SetStatus(newStatus);
+        //Assert
+        func.Should().Throw<DomainException>();
+    }
 
-    //    //Act 
+    [Fact]
+    public void SetStatus_From_InProgress_To_New_Fail()
+    {
+        //Arrange    
+        var task = new TaskEntity("Author", status: Status.InProgress);
+        //Act          
+        var func = () => task.SetStatus(Status.New);
+        //Assert
+        func.Should().Throw<DomainException>();
+    }
 
-    //    //Assert
-    //    Assert.IsNotNull();
-    //}
-
+    [Fact]
+    public void SetStatus_From_InProgress_To_Done_Success()
+    {
+        //Arrange    
+        var task = new TaskEntity("Author", status: Status.InProgress);
+        //Act          
+        task.SetStatus(Status.Done);
+        //Assert
+        task.Status.Should().Be(Status.Done);
+    }
 }
